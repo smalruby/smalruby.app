20856ca3e8990e5df2bc33e020322d6aa3f7f52e
takaokouji <takaokouji@users.noreply.github.com>
2026-03-28 23:55:12 +0000

    Build for 7dd1fefe27994314055bd717387a9f3f1244bff6 Merge pull request #408 from smalruby/feature/smalruby3-prettier
    chore: introduce prettier for Smalruby-specific files

--- types/reducers/smalruby-registry.d.ts
+++ types/reducers/smalruby-registry.d.ts
@@ -1,18 +1,4 @@
 /**
- * Smalruby-specific Redux state registry
- *
- * This file centralizes all Smalruby customizations to Redux state management.
- * By importing from this single registry file, we minimize merge conflicts when
- * updating from upstream scratch-gui.
- *
- * When adding new Smalruby reducers:
- * 1. Add the import statement here
- * 2. Add to smalrubyReducers object
- * 3. Add to smalrubyInitialState object
- *
- * No changes needed in gui.ts - it imports this registry.
- */
-/**
  * All Smalruby reducers
  * These will be spread into combineReducers() in gui.ts
  */
